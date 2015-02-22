79dd90ed5f48ae8ca56e1864b6df488f9a6e0894
Chris <ccarberry2@domain.com>
2015-02-21 18:56:51 -0600

    stuff

--- app/src/main/java/com/carberry/chris/ece498_mp1/MainActivity.java
+++ app/src/main/java/com/carberry/chris/ece498_mp1/MainActivity.java
@@ -1,98 +1,43 @@
 package com.carberry.chris.ece498_mp1;
 
-import android.hardware.Sensor;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.content.Intent;
-
-
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import android.widget.Textview;
+import android.widget.SeekBar;
 
-//for writing to csv file
 import java.io.FileWriter;
 import java.io.IOException;
+
+import static android.os.SystemClock.uptimeMillis;
+
+/* for writing to csv file */
 
 
 
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    // Chris -> I think this has to do with calling our stuff. onCreate should start up
-    // as soon as we start a new activity
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, MySensorActivity.class);
-    }
-
-
-}
-
-
-
-
-
-
-/********************************************************
- *              Begin sensor class
- *******************************************************/
-
-
-public class MySensorActivity extends MainActivity {
-
-    /*
     private Sensor mGyroSensor;
     private Sensor mLightSensor;
     private Sensor mAccelerometer;
     private Sensor Magnetometer;
-    */
+    private SensorManager mSensorManager;
+    private SensorEventListener mSensorListener;
+
 
     // pedometer tutorial
     // http://nebomusic.net/androidlessons/Pedometer_Project.pdf
     //values for PEDOMETER/STEPS
-    Private Button buttonReset;
+    private Button buttonReset;
     private float acceleration;
 
     //values to Calculate Number of Steps
@@ -106,6 +51,7 @@ public class MySensorActivity extends MainActivity {
 
 
     //values for csv file
+    float timeStamp;
     float Accel_x;
     float Accel_y;
     float Accel_z;
@@ -117,13 +63,21 @@ public class MySensorActivity extends MainActivity {
     float Mag_z;
     float Light_intensity;
 
-    private SensorManager mSensorManager;
-    private SensorEventListener mSensorListener;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mGyroSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        Magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        //mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+        //mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
+        //mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
+        //mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_GAME);
 
         threshold = 10;
         previousY = 0;
@@ -132,8 +86,8 @@ public class MySensorActivity extends MainActivity {
 
         // This is what we found on the stack overflow, something is screwed up though, context might be incorrect
         //http://stackoverflow.com/questions/4343342/is-there-a-way-to-retrieve-multiple-sensor-data-in-android
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
+        timeStamp = uptimeMillis();     //time since system boot
 
 
         //private inner class
@@ -150,8 +104,230 @@ public class MySensorActivity extends MainActivity {
 
                     /*logic for pedometer -> # of steps */
 
+                    /* fetch the current y */
+                    currentY = Accel_y;
+
+                    //Measure if a step is taken
+                    if (Math.abs(currentY - previousY) > threshold){
+                        numSteps++;
+                    }
+
+                    // store previous y
+                    previousY = currentY;
+
+                } if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                    Gyro_x = event.values[0];
+                    Gyro_y = event.values[1];
+                    Gyro_z = event.values[2];
+                } if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+                    Mag_x = event.values[0];
+                    Mag_y = event.values[1];
+                    Mag_z = event.values[2];
+                } if (sensor.getType() == Sensor.TYPE_LIGHT) {
+                    Light_intensity = event.values[0];
+                }
+            }
+
+            //@Override
+            //  public void onAccuracyChanged(Sensor arg0, int arg1) {
+                //
+            // }
+        };
+
+        /*
+        // USE THIS FOR CHECKING IF PHONE HAS THE FEATURE
+        PackageManager PM= this.getPackageManager();
+        boolean gyro = PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE);
+        boolean light = PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_LIGHT);
+        */
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        /*
+            Handle action bar item clicks here. The action bar will
+            automatically handle clicks on the Home/Up button, so long
+            as you specify a parent activity in AndroidManifest.xml.
+        */
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for each sensor.
+        super.onResume();
+        mSensorManager.registerListener((SensorEventListener) this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener((SensorEventListener) this, mGyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener((SensorEventListener) this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener((SensorEventListener) this, Magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // important to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+    /*
+    first attempt at writing csv, this may need to take an existing file and append to it that way
+    depending on how timing works with the sensors
+    */
+    public void generateCsvFile(String sFilename)
+    {           //call this function onClick (start)
+        try
+        {
+            FileWriter writer = new FileWriter(sFilename);
+            boolean i = true;
+            // Need to fix this while loop with a button?
+            while(i) { //while !onClick(Stop)    race condition?
+                writer.append(Float.toString(timeStamp));
+                writer.append(',');
+                writer.append(Float.toString(Accel_x));
+                writer.append(',');
+                writer.append(Float.toString(Accel_y));
+                writer.append(',');
+                writer.append(Float.toString(Accel_z));
+                writer.append(',');
+                writer.append(Float.toString(Gyro_x));
+                writer.append(',');
+                writer.append(Float.toString(Gyro_y));
+                writer.append(',');
+                writer.append(Float.toString(Gyro_z));
+                writer.append(',');
+                writer.append(Float.toString(Mag_x));
+                writer.append(',');
+                writer.append(Float.toString(Mag_y));
+                writer.append(',');
+                writer.append(Float.toString(Mag_z));
+                writer.append(',');
+                writer.append(Float.toString(Light_intensity));
+                writer.append('\n');
+
+            }
+
+            //generate whatever data you want
+
+            writer.flush();
+            writer.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Chris -> I think this has to do with calling our stuff. onCreate should start up
+    // as soon as we start a new activity
+    /*public void sendMessage(View view) {
+        Intent intent = new Intent(this, MySensorActivity.class);
+    }*/
+}
+
+
+
+
+
+
+
+
+
+
+
+/********************************************************
+ *              Begin sensor class
+ *******************************************************/
+
+/*
+class MySensorActivity extends MainActivity {
+
+
+    private Sensor mGyroSensor;
+    private Sensor mLightSensor;
+    private Sensor mAccelerometer;
+    private Sensor Magnetometer;
+
+
+    // pedometer tutorial
+    // http://nebomusic.net/androidlessons/Pedometer_Project.pdf
+    //values for PEDOMETER/STEPS
+    private Button buttonReset;
+    private float acceleration;
+
+    //values to Calculate Number of Steps
+    private float previousY;
+    private float currentY;
+    private int numSteps;
+
+    // SeekBar Fields
+    private SeekBar seekbar;
+    private int threshold;
+
+
+    //values for csv file
+    float timeStamp;
+    float Accel_x;
+    float Accel_y;
+    float Accel_z;
+    float Gyro_x;
+    float Gyro_y;
+    float Gyro_z;
+    float Mag_x;
+    float Mag_y;
+    float Mag_z;
+    float Light_intensity;
+
+    private SensorManager mSensorManager;
+    private SensorEventListener mSensorListener;
+*/
+/*    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        threshold = 10;
+        previousY = 0;
+        currentY = 0;
+        numSteps = 0;
+
+        // This is what we found on the stack overflow, something is screwed up though, context might be incorrect
+        //http://stackoverflow.com/questions/4343342/is-there-a-way-to-retrieve-multiple-sensor-data-in-android
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        timeStamp = uptimeMillis();     //time since system boot
+
+
+        //private inner class
+        mSensorListener = new SensorEventListener()
+        {
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                Sensor sensor = event.sensor;
+                if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                    Accel_x = event.values[0];
+                    Accel_y = event.values[1];
+                    Accel_z = event.values[2];
+
+                    //logic for pedometer -> # of steps
+
                     // fetch the current y
-                    currentY = accel_y;
+                    currentY = Accel_y;
 
                     //Measure if a step is taken
                     if (Math.abs(currentY - previousY) > threshold){
@@ -159,7 +335,7 @@ public class MySensorActivity extends MainActivity {
                     }
 
                     // store previous y
-                    previousY = y;
+                    previousY = currentY;
 
                 } if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
                     Gyro_x = event.values[0];
@@ -185,40 +361,42 @@ public class MySensorActivity extends MainActivity {
         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
         mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT), SensorManager.SENSOR_DELAY_GAME);
 
-        /*
+
         // USE THIS FOR CHECKING IF PHONE HAS THE FEATURE
-        PackageManager PM= this.getPackageManager();
-        boolean gyro = PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE);
-        boolean light = PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_LIGHT);
-        */
+        //PackageManager PM= this.getPackageManager();
+        //boolean gyro = PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_GYROSCOPE);
+        //boolean light = PM.hasSystemFeature(PackageManager.FEATURE_SENSOR_LIGHT);
 
-    }
 
-    @Override
-    protected void onResume() {
+    }*/
+
+/*    @Override
+    protected void onStart() {
         // Register a listener for the sensor.
-        super.onResume();
-        mSensorManager.registerListener(this, mGyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        super.onStart();
+        mSensorManager.registerListener((SensorEventListener) this, mGyroSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         // important to unregister the sensor when the activity pauses.
         super.onPause();
-        mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener((SensorEventListener) this); //not sure if correct
     }
 
-    // first attempt at writing csv, this may need to take an existing file and append to it that way
-    // depending on how timing works with the sensors
+    //
+    //first attempt at writing csv, this may need to take an existing file and append to it that way
+    //depending on how timing works with the sensors
+
     public void generateCsvFile(String sFilename)
-    {
+    {           //call this function onClick (start)
         try
         {
-            FileWriter writer = new FileWriter(sFileName);
+            FileWriter writer = new FileWriter(sFilename);
 
             // Need to fix this while loop with a button?
-            while(1) {
-                writer.append(Float.toString(timestamp));
+            while(1) { //while !onClick(Stop)    race condition?
+                writer.append(Float.toString(timeStamp));
                 writer.append(',');
                 writer.append(Float.toString(Accel_x));
                 writer.append(',');
@@ -256,3 +434,4 @@ public class MySensorActivity extends MainActivity {
     }
 
 }
+*/
