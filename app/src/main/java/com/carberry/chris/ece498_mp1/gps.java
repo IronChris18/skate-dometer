c78680a0c2aac1cbe698333e42b1856254ababf2
Chris <ccarberry2@domain.com>
2015-05-03 21:23:31 -0500

    try fused location stuff

--- app/src/main/java/com/carberry/chris/ece498_mp1/gps.java
+++ app/src/main/java/com/carberry/chris/ece498_mp1/gps.java
@@ -1,64 +1,198 @@
 package com.carberry.chris.ece498_mp1;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class gps extends FragmentActivity implements LocationListener{
+import java.text.DateFormat;
+import java.util.Date;
+
+class gps extends FragmentActivity{
+    private static final String TAG = "LocationActivity";
+    private static final long INTERVAL = 1000 * 10;
+    private static final long FASTEST_INTERVAL = 1000 * 5;
+    LocationRequest mLocationRequest;
+    GoogleApiClient mGoogleApiClient;
+    Location mCurrentLocation;
+    String mLastUpdateTime;
     double latitude;
     double longitude;
+    int flag = 0;
+    LatLng prev = new LatLng(0,0);
     private LocationManager locationManager;
-    private static final long MIN_TIME = 500;   //update every 0.5 seconds
-    private static final float MIN_DISTANCE = 0;//update after 5 meters
+    private static final long MIN_TIME = 1000;   //update in milliseconds
+    private static final float MIN_DISTANCE = 10;//update in meters
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(INTERVAL);
+        mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate ...............................");
+        //show error dialog if GoolglePlayServices not available
+        if (!isGooglePlayServicesAvailable()) {
+            finish();
+        }
+        createLocationRequest();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks((GoogleApiClient.ConnectionCallbacks) this)
+                .addOnConnectionFailedListener((GoogleApiClient.OnConnectionFailedListener) this)
+                .build();
         setContentView(R.layout.activity_gps);
 
         setUpMapIfNeeded();
 
-
-
-        mMap.setMyLocationEnabled(true);
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, (LocationListener) this);
+        //mMap.setMyLocationEnabled(true);
+        //mMap.setOnMyLocationChangeListener(myLocationChangeListener);
+        //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, (LocationListener) this);
     }
 
-    @Override
+   /* @Override
     protected void onResume() {
         super.onResume();
         setUpMapIfNeeded();
+    }*/
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
     }
     @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
+    }
+    private boolean isGooglePlayServicesAvailable() {
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (ConnectionResult.SUCCESS == status) {
+            return true;
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(status, this, 0).show();
+            return false;
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.d(TAG, "onConnected - isConnected ...............: " + mGoogleApiClient.isConnected());
+        startLocationUpdates();
+    }
+
+    protected void startLocationUpdates() {
+        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+        Log.d(TAG, "Location update started ..............: ");
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, "Connection failed: " + connectionResult.toString());
+    }
+
+    @Override
     public void onLocationChanged(Location location) {
-        latitude  = location.getLatitude();
-        longitude = location.getLongitude();
-        LatLng LOCAL_VIEW = new LatLng(latitude, longitude);
+        Log.d(TAG, "Firing onLocationChanged..............................................");
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+    }
 
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(LOCAL_VIEW, 19);
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, (com.google.android.gms.location.LocationListener) this);
+        Log.d(TAG, "Location update stopped .......................");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient.isConnected()) {
+            startLocationUpdates();
+            Log.d(TAG, "Location update resumed .....................");
+        }
+    }
+
+
+/*    private GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
+        @Override
+        public void onMyLocationChange(Location location) {
+            LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+            Marker mMarker = mMap.addMarker(new MarkerOptions().position(loc));
+            if(flag==0)  //when the first update comes, we have no previous points,hence this
+            {
+                prev=loc;
+                flag=1;
+            }
+            mMap.addPolyline((new PolylineOptions())
+                    .add(prev, loc).width(6).color(Color.BLUE)
+                    .visible(true));
+
+            prev=loc;
+            if(mMap != null){
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc, 19.0f));
+            }
+        }
+    };*/
+    /*
+    @Override
+    public void onLocationChanged(Location location) {
+        //latitude  = location.getLatitude();
+        //longitude = location.getLongitude();
+        LatLng current = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if(flag==0)  //when the first update comes, we have no previous points,hence this
+        {
+            prev=current;
+            flag=1;
+        }
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(current, 19);
         mMap.animateCamera(update);
-        /*CameraPosition cameraPosition = new CameraPosition.Builder()
-                .target(LOCAL_VIEW )      // Sets the center of the map to Mountain View
-                .zoom(19)                   // Sets the zoom
-                .bearing(0)                // Sets the orientation of the camera to east
-                .tilt(30)                   // Sets the tilt of the camera to 30 degrees
-                .build();                   // Creates a CameraPosition from the builder
-        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-        locationManager.removeUpdates(this);*/
+        mMap.addPolyline((new PolylineOptions())
+                .add(prev, current).width(6).color(Color.BLUE)
+                .visible(true));
 
+        prev=current;
+        current = null;
     }
 
     @Override
@@ -77,7 +211,7 @@ public class gps extends FragmentActivity implements LocationListener{
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
 
-    }
+    }*/
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
      * installed) and the map has not already been instantiated.. This will ensure that we only ever
